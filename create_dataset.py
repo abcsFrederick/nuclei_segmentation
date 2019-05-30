6e1920d5e3eb047e50f05e7df66879bfb0f21b36
Christian Suloway <christian.suloway@nih.gov>
2019-05-30 09:04:44 -0400

    create test dataset

--- create_dataset.py
+++ create_dataset.py
@@ -4,17 +4,26 @@ import random
 from tfrecord.writer import LabeledImageTFRecordWriter
 
 
-def main(datasets, filename, validation_filename=None, validation_split=None,
-         random_seed=None):
+def main(datasets, filename, test_filename=None, test_split=None,
+         validation_filename=None, validation_split=None, random_seed=None):
     writer = LabeledImageTFRecordWriter(filename)
 
+    indices = set()
+    for i, dataset in enumerate(datasets):
+        for j in range(len(dataset)):
+            indices.add((i, j))
+
+    if test_filename and test_split:
+        test_size = math.ceil(test_split*len(indices))
+        test_indices = set(random.sample(indices, test_size))
+        indices -= test_indices
+        test_writer = LabeledImageTFRecordWriter(test_filename)
+    else:
+        test_indices = []
+
     if validation_filename and validation_split:
-        indices = []
-        for i, dataset in enumerate(datasets):
-            for j in range(len(dataset)):
-                indices.append((i, j))
         validation_size = math.ceil(validation_split*len(indices))
-        validation_indices = random.sample(indices, validation_size)
+        validation_indices = set(random.sample(indices, validation_size))
         validation_writer = LabeledImageTFRecordWriter(validation_filename)
     else:
         validation_indices = []
@@ -23,7 +32,10 @@ def main(datasets, filename, validation_filename=None, validation_split=None,
         name = dataset.__class__.__name__
         for j, values in enumerate(dataset.labeled_images):
             identifier, (image, label) = values
-            if (i, j) in validation_indices:
+            index = (i, j)
+            if index in test_indices:
+                w = test_writer
+            elif index in validation_indices:
                 w = validation_writer
             else:
                 w = writer
@@ -51,6 +63,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('filename')
     parser.add_argument('--all')
+    parser.add_argument('--test_filename')
+    parser.add_argument('--test_split', type=float)
     parser.add_argument('--validation_filename')
     parser.add_argument('--validation_split', type=float)
     parser.add_argument('--random_seed', type=int)
@@ -72,5 +86,6 @@ if __name__ == '__main__':
     if not datasets:
         sys.exit(2)
 
-    main(datasets, arguments.filename, arguments.validation_filename,
+    main(datasets, arguments.filename, arguments.test_filename,
+         arguments.test_split, arguments.validation_filename,
          arguments.validation_split)
